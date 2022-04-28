d68cbf54de36035ae9cc2fc69f51838820078668
DongukHam <hdw8607@gmail.com>
2022-04-28 12:48:11 +0900

    Created index

---
+++ app.js
@@ -0,0 +1,11 @@
+const a= 10 , b = 2;
+let myName='David Ham';
+
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+console.log("hello " + myName);
+
+myName = "Nicola"
+
+console.log("My new name is " + myName);
