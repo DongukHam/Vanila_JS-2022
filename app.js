ec76d77ba6c25151d74eb24eb0f9d6208791a9bc
DongukHam <hdw8607@gmail.com>
2022-05-03 12:44:10 +0900

    updated

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
-const a= 10 , b = 2;
-let myName='David Ham';
+// const a= 10 , b = 2;
+// let myName='David Ham';
 
-console.log(a+b);
-console.log(a*b);
-console.log(a/b);
-console.log("hello " + myName);
+// console.log(a+b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log("hello " + myName);
 
-myName = "Nicola"
+// myName = "Nicola"
 
-console.log("My new name is " + myName);
+// console.log("My new name is " + myName);
+
+
+// const amIFat = null;
+// let something;
+// console.log(something, amIFat);
+
+// [array]
+const daysOfWeek = ["mon","tue","wed","thu","fri","sat","sun"]
+
+console.log(daysOfWeek);
+// get item from array
+console.log(daysOfWeek[5]);
+// add one more
+daysOfWeek[7] = "holiday";
+daysOfWeek.push("lotto");
+console.log(daysOfWeek);
